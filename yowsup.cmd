15f53997c43e0831daa268d119ced577d432106c
gaudat <me@gaudat.name>
2016-04-14 13:25:35 +0800

    Added yowsup library

---
+++ yowsup.cmd
@@ -0,0 +1,9 @@
+/L
+/state typing 85268794568-1453974029@g.us
+/wait 1.2
+/state paused 85268794568-1453974029@g.us
+/wait 0.5
+/message send 85268794568-1453974029@g.us "I'm self conscious"
+/wait 1
+/disconnect
+/exit
