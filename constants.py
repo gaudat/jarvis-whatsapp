83a111f2c9b7a6baff447c5dfc49c67322deb7eb
gaudat <me@gaudat.name>
2016-04-21 14:51:31 +0800

    For paul

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ yowsup_conf = "/home/me/jarvis-whatsapp/yowsup.conf"
 yowsup_args = [yowsup_cli,"demos","-y","-e","-c",yowsup_conf]
 timeout = 5
 group_jid = "85268794568-1453974029@g.us"
-
+flush = True
 
 hostname = 'ja-cdbr-azure-east-a.cloudapp.net'
 user = 'b34334f04097e9'
@@ -16,3 +16,7 @@ magic_word = 'jarvis'
 
 aircon_on_threshold = 25
 dehum_on_threshold = 80
+
+import colour
+color = colour.Color
+
--- interface.py
+++ interface.py
@@ -31,6 +31,8 @@ def receive_messages():
     return (o,e)
 
 def send_message(message,recipient=group_jid):
+    if flush:
+        return
     p = subprocess.Popen(yowsup_args,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     o,e = p.communicate(input=get_cmd_send_message(recipient,message))
     return (o,e)
